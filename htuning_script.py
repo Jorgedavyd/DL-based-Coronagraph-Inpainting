8fbe21e216b23b0b72273d1b37db749ec8a7e55b
Jorgedavyd <jorged.encyso@gmail.com>
2024-05-05 15:55:02 -0400

    htuning failed, lightning hpo unable to be set

--- htuning_script.py
+++ htuning_script.py
@@ -9,8 +9,8 @@ from lightning_hpo.distributions.distributions import LogUniform
 app = LightningApp(
     Sweep(
         script_path=ops.join(ops.dirname(__file__), "./random_search.py"),
-        total_experiments=3,
-        parallel_experiments=1,
+        total_experiments=50,
+        parallel_experiments=2,
         distributions={
             "--model.encoder_wd": LogUniform(1e-6, 1e-3),
             "--model.encoder_lr": LogUniform(1e-7, 1e-3),
